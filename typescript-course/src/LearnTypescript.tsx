93071a9b1c825398bbdba8a34536fa5db7d99b38
akousa1 <akousa47@gmail.com>
2026-08-13 13:35:01 +1000

    ts changes

--- typescript-course/src/LearnTypescript.tsx
+++ typescript-course/src/LearnTypescript.tsx
@@ -2,114 +2,144 @@ import React from "react";
 
 const LearnTypescript = () => {
   // Basic Types
-let id: number = 5
-let company: string = 'Traversy Media'
-let isPublished: boolean = true
-let x: any = 'Hello'
+  let id: number = 5;
+  // id = '5'; // Throws error as type string not assignable to type number
+  id = 5; // Works fine
+  console.log("Id: ", id); //ID: 5
 
-let ids: number[] = [1, 2, 3, 4, 5]
-let arr: any[] = [1, true, 'Hello']
+  let company: string = "Albertkousa";
+  console.log("Company: ", company); //Company: Albertkousa
 
-// Tuple
-let person: [number, string, boolean] = [1, 'Brad', true]
-// Tuple Array
-let employees: [number, string][]
+  let isPublished: boolean = true;
+  console.log("Is Published: ", isPublished); //Is Published: true
 
-employees = [
-  [1, 'Brad'],
-  [2, 'John'],
-  [3, 'Jill'],
-]
+  let x: any = "Hello";
+  console.log("X: ", x);
 
-// Union
-let pid: string | number
-pid = '22'
+  let ids: number[] = [1, 2, 3, 4, 5];
+  console.log("IDs: ", ids); //IDs: [1, 2, 3, 4, 5]
+  //ids.push('6'); // Throws error as type string not assignable to type number
+  let arr: any[] = [1, true, "Hello"];
+  console.log("Arr: ", arr); //Arr: [1, true, "Hello"]
 
-// Enum
-enum Direction1 {
-  Up = 1,
-  Down,
-  Left,
-  Right,
-}
+  // Tuple
+  let person: [number, string, boolean] = [1, "Brad", true];
+  console.log("Person: ", person); //Person: [1, "Brad", true]
 
-enum Direction2 {
-  Up = 'Up',
-  Down = 'Down',
-  Left = 'Left',
-  Right = 'Right',
-}
+  // Tuple Array
+  let employee: [number, string][];
+  employee = [
+    [1, "Brad"],
+    [2, "John"],
+    [3, "Jill"],
+  ];
+  console.log("Employee: ", employee);
+  //Employee: [[1, "Brad"], [2, "John"], [3, "Jill"]]
 
-// Objects
-type User = {
-  id: number
-  name: string
-}
+  // Union
+  let pid: string | number = 22;
+  pid = "22";
+  console.log("PID: ", pid); //PID: 22
 
-const user: User = {
-  id: 1,
-  name: 'John',
-}
-
-// Type Assertion
-let cid: any = 1
-// let customerId = <number>cid
-let customerId = cid as number
-
-// Functions
-function addNum(x: number, y: number): number {
-  return x + y
-}
-
-// Void
-function log(message: string | number): void {
-  console.log(message)
-}
-
-// Interfaces
-interface UserInterface {
-  readonly id: number
-  name: string
-  age?: number
-}
-
-const user1: UserInterface = {
-  id: 1,
-  name: 'John',
-}
-
-interface MathFunc {
-  (x: number, y: number): number
-}
-
-const add: MathFunc = (x: number, y: number): number => x + y
-const sub: MathFunc = (x: number, y: number): number => x - y
-
-interface PersonInterface {
-  id: number
-  name: string
-  register(): string
-}
-
-// Classes
-class Person implements PersonInterface {
-  id: number
-  name: string
-
-  constructor(id: number, name: string) {
-    this.id = id
-    this.name = name
+  // Enum
+  enum Direction1 {
+    Up = "Up",
+    Down = "Down",
+    Left = "Left",
+    Right = "Right",
   }
+  console.log("Direction1: ", Direction1.Up); //Direction1: Up
+  //Below variables will have been set to their respective string values
 
-  register() {
-    return `${this.name} is now registered`
+  // Objects
+  type User = {
+    id: number;
+    name: string;
+  }; //This is a type alias for an object with id and name properties
+  const user: User = {
+    id: 1,
+    name: "John",
+  }; //This is an object of type User
+
+  // Type Assertion
+  let cid: any = 1;
+  let customerId = cid as number; //This is a type assertion that tells
+  // the compiler that cid is of type number
+
+  //customerId = '1'; // Throws error as type string not assignable to
+  // type number
+
+  // Functions
+  function addnum(x: number, y: number): number {
+    return x + y;
+  } //This is a function that takes two parameters x and y of type number
+  // and returns a number
+  console.log("Add Num: ", addnum(1, 2)); //Add Num: 3
+
+  // Void
+  function log(message: string | number): void {
+    console.log(message);
+  } //This is a function that takes a parameter message of type string
+  //or number
+
+  log("Hello World"); //Hello World
+
+  // Interfaces
+  interface UserInterface {
+    readonly id: number; //This property is read-only and cannot be 
+    //changed
+    name: string;
+    age?: number; //this property is optional and may or may not be
+    //present
+  } //This is an interface that defines the shape of an object with id
+  //and name properties
+
+  const user1: UserInterface = {
+    id: 1,
+    name: "John",
+  }; //This is an object of type UserInterface
+
+  interface MathFunc {
+    (x: number, y: number): number; //This is a function that takes two parameters x and y of type number
+  } //and returns a number
+
+  const add: MathFunc = (x: number, y: number): number => x + y; //This is
+  //a function that takes two parameters x and y of type number
+  const sub: MathFunc = (x: number, y: number): number => x - y; //This is
+  //a function that takes two parameters x and y of type number
+
+  interface PersonInterface {
+    id: number;
+    name: string;
+    age?: number; //this property is optional and may or may not be
+    //present
   }
-}
+  // Classes
+  class Person {
+    id: number;
+    //if i want to make the name property private,
+    //  i can use the private keyword and it will not be accessible
+    // outside the class
+    //i can also use the protected keyword and it will be
+    // accessible only within the class and its subclasses
+    name: string;
 
-const brad = new Person(1, 'Brad Traversy')
-const mike = new Person(2, 'Mike Jordan')
+    constructor(id: number, name: string) {
+      this.id = id;
+      this.name = name;
+    }
+    register() {
+      return `${this.name} is now registered`;
+    }
+  }
+  const albert = new Person(1, "Albert Kousa");
+  const john = new Person(2, "John Doe");
 
-// Subclasses
+  console.log("Albert: ", albert); //Albert: Person {id: 1, name: "Albert Kousa"}
+  console.log("John: ", john); //John: Person {id: 2, name: "John Doe"}
+
+  console.log(albert.register()); //Albert Kousa is now registered
+  // Subclasses
 class Employee extends Person {
   position: string
 
